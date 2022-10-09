328e1bbea21574a52afc95f809786e7f7b423a2c
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-09 14:27:49 +0530

    basic css added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Rock from "./icons/Rock";
+import paper from "./icons/paper";
+import Scissors from "./icons/Scissors";
+
 import './App.css'
 
 function App()
 {
     return (
-        <div>
-            hi
+        <div className="app">
+            
         </div>
     )
 }
