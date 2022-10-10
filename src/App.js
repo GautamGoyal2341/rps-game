e993a46a757bfebd78a18c5a9d6d71bad9583629
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-10 14:48:39 +0530

    icon without css setuped

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
 import React from "react";
 import Rock from "./icons/Rock";
-import paper from "./icons/paper";
+import Paper from "./icons/Paper";
 import Scissors from "./icons/Scissors";
 
-import './App.css'
+import "./App.css";
 
-function App()
-{
-    return (
-        <div className="app">
-            <div className="info">
+function App() {
+  return (
+    <div className="app">
+      <div className="info">
+        <h2>Rock . Paper . Scissors</h2>
 
-                <h2>Rock . Paper . Scissors</h2>
+        <div className="win-losses">
+          <div className="wins">
+            <span className="number">0</span>
+            <span className="text">Wins</span>
+          </div>
 
-
-
-                <div className="win-losses">
-                    <div className="wins">
-                        <span className="number">0</span>
-                        <span className="text">Wins</span>
-
-                    </div>
-
-
-                    <div className="losses">
-                        <span className="number">0</span>
-                        <span className="text">Losses</span>
-                    </div>
-
-
-
-                </div>
-
-            </div>
+          <div className="losses">
+            <span className="number">0</span>
+            <span className="text">Losses</span>
+          </div>
         </div>
-    )
+
+         <div  className="choices">
+            
+             <div>You</div>
+             <div>Computer</div>
+
+             
+         <div>
+             <button className="rock">
+                <Rock/>
+             </button>
+
+             <button className="paper">
+                <Paper/>
+             </button>
+             <button className="scissors">
+                <Scissors/>
+                
+             </button>
+
+         </div>
+
+
+         </div>
+
+
+
+
+      </div>
+    </div>
+  );
 }
 
-export default App; 
+export default App;
