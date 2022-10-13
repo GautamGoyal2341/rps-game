f35814e6d95a7b515c46632a91b8de8d03780a8b
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-10-13 15:57:52 +0530

    result component added

--- src/App.js
+++ src/App.js
@@ -5,33 +5,37 @@ import Scissors from "./icons/Scissors";
 import "./App.css";
 
 const choices = [
-  { id: 1, name: "rock", components: Rock },
-  { id: 2, name: "paper", components: Paper },
-  { id: 3, name: "scissor", components: Scissors },
+  { id: 1, name: "rock", component: Rock },
+  { id: 2, name: "paper", component: Paper },
+  { id: 3, name: "scissor", component: Scissors },
 ];
 
+   
+  function renderComponent(choice)
+  {
+    const Component = choice.component;
+    return <Component/>
+  }
+
+
+   
 function App() {
-    const [wins , setWins] = React.useState(0);
-    const [looses , setLooses] = React.useState(0);
-    const [userChoice , setUserChoice] = React.useState(null);
-    const [computerChoice , setComputerChoice] = React.useState(null);
-    const [gameState , setGameState] = React.useState(null);
+  const [wins, setWins] = React.useState(0);
+  const [looses, setLooses] = React.useState(0);
+  const [userChoice, setUserChoice] = React.useState(null);
+  const [computerChoice, setComputerChoice] = React.useState(null);
+  const [gameState, setGameState] = React.useState(null);
   function handelUserChoice(choice) {
     const chosenChoice = choices.find((c) => c.id === choice);
-      setUserChoice(chosenChoice);
+    setUserChoice(chosenChoice);
 
-
-      setGameState('win');
-
-
-
-
+    setGameState("win");
   }
 
   React.useEffect(() => {
     const randomChoice = choices[Math.floor(Math.random() * choices.length)];
     setComputerChoice(randomChoice);
-  } , []);
+  }, []);
 
   return (
     <div className="app">
@@ -41,28 +45,29 @@ function App() {
         <div className="wins-losses">
           <div className="wins">
             <span className="number">{wins}</span>
-            <span className="text">{ wins===1 ? "Win" : "Wins"  }</span>
+            <span className="text">{wins === 1 ? "Win" : "Wins"}</span>
           </div>
 
           <div className="losses">
             <span className="number">{looses}</span>
-            <span className="text">{ looses===1 ? "Loose" : "Looses"  }</span>
+            <span className="text">{looses === 1 ? "Loose" : "Looses"}</span>
           </div>
         </div>
       </div>
 
+      {gameState && (
+        <div className={`game-state ${gameState}`}>
+          <div className="game-state-content">
+
+            <p></p>
+            <p></p>
+            <p></p>
 
 
-  { gameState &&       <div className = {`game-state ${gameState}`} >
 
-                    <div className="game-state-content">
-
-
-
-                    </div>
- 
+          </div>
         </div>
-}
+      )}
 
       <div className="choices">
         <div>You</div>
@@ -73,10 +78,10 @@ function App() {
           <button className="rock" onClick={() => handelUserChoice(1)}>
             <Rock />
           </button>
-          <button className="paper" onClick={ () => handelUserChoice(2)}>
+          <button className="paper" onClick={() => handelUserChoice(2)}>
             <Paper />
           </button>
-          <button className="scissors" onClick={ () => handelUserChoice(3)}>
+          <button className="scissors" onClick={() => handelUserChoice(3)}>
             <Scissors />
           </button>
         </div>
